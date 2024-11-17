01cdc80849b94975dea3914b375bea6bb7ded557
MMoose16 <1mieszko@sbcglobal.net>
2024-11-17 05:34:54 -0600

    Data Sending Preparation Part 2

--- VR Project Hackathon 2024/Assets/Scripts/DataSenderAct2.cs
+++ VR Project Hackathon 2024/Assets/Scripts/DataSenderAct2.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.Windows;
 using File = System.IO.File;
 
 public class DataSenderAct2 : MonoBehaviour
 {
+    private string url = "http://localhost:5000/generate";
     public CompareDistance comds;
     public TextMeshProUGUI id;
     private string[] planetNames = new[] { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
@@ -16,8 +19,11 @@ public class DataSenderAct2 : MonoBehaviour
     {
         Directory.CreateDirectory("Assets/Data");
     }
-
-    public void CreateDataFile()
+    public void SendDataToDatabase()
+    {
+        StartCoroutine(DataSend());
+    }
+    public IEnumerator DataSend()
     {
         SendData data = new SendData();
         data.userID = id.text;
@@ -28,6 +34,23 @@ public class DataSenderAct2 : MonoBehaviour
             data.planetValues.Add(new PlanetData(planetNames[i], comds.GetEstimatePositions()[i], comds.GetErrors()[i])); 
         }
         string json = JsonUtility.ToJson(data, true);
-        File.WriteAllText(Application.dataPath + $"/Data/{data.userID}.json", json);
+        //File.WriteAllText(Application.dataPath + $"/Data/{data.userID}.json", json);
+        
+        using (UnityWebRequest request = new UnityWebRequest(url, "POST"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Response: " + request.downloadHandler.text);
+            }
+            else
+            {
+                Debug.Log("Error: " + request.error);
+            }
+        }
     }
 }
